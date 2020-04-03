0eea149a8c4dc4edabf9f8aa96fdfa35d0ced64f
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-04-03 15:50:01 +0200

    Fix indentation

--- src/data/connect.py
+++ src/data/connect.py
@@ -1,18 +1,18 @@
 from pymongo import MongoClient
 
 def connect_mongodb_database(host, port, database, username, password):
-    """
-    Connect to the given MongoDB-database.
+  """
+  Connect to the given MongoDB-database.
 
-    Parameters
-    ----------
-    - host -- The host where MongoDB is running.
-    - port -- The port on which MongoDB listens.
-    - database -- The database to connect to.
+  Parameters
+  ----------
+  - host -- The host where MongoDB is running.
+  - port -- The port on which MongoDB listens.
+  - database -- The database to connect to.
 
-    Returns: a reference to the requested database.
-    """
-    url = 'mongodb://{}:{}@{}:{}/{}?authSource=admin&authMechanism=SCRAM-SHA-1'.format(username, password, host, port, database)
-    print('=> Connected to MongoDB on {}:{}/{}'.format(host, port, database))
-    mongoClient = MongoClient(url)
-    return mongoClient[database]
+  Returns: a reference to the requested database.
+  """
+  url = 'mongodb://{}:{}@{}:{}/{}?authSource=admin&authMechanism=SCRAM-SHA-1'.format(username, password, host, port, database)
+  print('=> Connected to MongoDB on {}:{}/{}'.format(host, port, database))
+  mongoClient = MongoClient(url)
+  return mongoClient[database]
