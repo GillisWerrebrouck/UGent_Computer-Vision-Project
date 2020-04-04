c0222966ce4ebe5333ea9eb37b609209ffb14b6d
Thomas Aelbrecht <thomasaelbrecht@live.com>
2020-04-04 12:28:54 +0200

    Add possible candidate for detecting paintings.

--- src/core/detection.py
+++ src/core/detection.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from core.filter import apply_grayscale_for_painting_detection
+from core.visualize import show_image
 
 def detect_corners(image):
   """
@@ -31,7 +33,7 @@ def detect_corners2(image):
   dst = cv2.cornerHarris(grayscale, 15,15,0.1)
   #result is dilated for marking the corners, not important
   # dst = cv2.dilate(dst,None)
-  
+
   kernel = np.ones((5,5), np.uint8)
   dst = cv2.dilate(dst, kernel, iterations=1)
 
@@ -42,9 +44,41 @@ def detect_corners2(image):
   image = cv2.medianBlur(grayscale, 27)
   image = cv2.Canny(image, threshold1=50, threshold2=150, apertureSize=3)
   dst = cv2.dilate(dst, kernel, iterations=1)
-  contours, hierarchy = cv2.findContours(image, mode = cv2.RETR_EXTERNAL, method = cv2.CHAIN_APPROX_SIMPLE)
+  contours, hierarchy = cv2.findContours(image, mode = cv2.RETR_CCOMP, method = cv2.CHAIN_APPROX_SIMPLE)
 
   for c in contours:
     x, y, w, h = cv2.boundingRect(c)
     original = cv2.rectangle(original, (x, y), (x+w, y+h), (0, 255, 0), 14)
   image=original
+
+
+def detect_corners3(image):
+  original = image
+
+  (rows, cols) = image.shape[:2]
+  minHeight = rows / 32.0
+  minWidth = cols / 32.0
+
+  imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+  kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+  imgray = cv2.dilate(imgray, kernel, iterations=1)
+  imgray = cv2.erode(imgray, kernel, iterations=2)
+
+  imgray = cv2.medianBlur(imgray, 13)
+  ret, thresh = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY)
+
+  canny = cv2.Canny(thresh, threshold1=50, threshold2=150, apertureSize=3)
+
+  kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (11, 11))
+  canny = cv2.dilate(canny, kernel)
+  contours, hierarchy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+  # original = cv2.drawContours(original, contours, -1, (0,255,0), 3)
+  for c in contours:
+    x, y, w, h = cv2.boundingRect(c)
+
+    if (w >= minWidth and h >= minHeight):
+      original = cv2.rectangle(original, (x, y), (x+w, y+h), (0, 255, 0), 14)
+
+  return original
--- src/index.py
+++ src/index.py
@@ -3,8 +3,9 @@ import sys
 import glob
 from data.connect import connect_mongodb_database
 from core.visualize import show_image, resize_image
-from core.detection import detect_corners, detect_corners2
-from core.filter import apply_filters
+from core.detection import detect_corners, detect_corners2, detect_corners3
+from core.filter import apply_grayscale_for_painting_detection
+from glob import glob
 
 mydb = connect_mongodb_database('localhost', 27017, 'computervision', 'devuser', 'devpwd')
 print(mydb.list_collection_names())
@@ -12,10 +13,9 @@ print(mydb.list_collection_names())
 if (mydb == None):
   exit(-1)
 
-for filename in glob.glob('./dataset_pictures_msk/*/*.jpg'):
+# Difficult one: ../../data/images/dataset_pictures_msk/Zaal_A/20190323_111950.jpg
+for filename in sorted(glob('../../data/images/dataset_pictures_msk/**/*.jpg')):
   img = cv2.imread(filename)
-  img = apply_filters(img)
-  # detect_corners(img)
-  # detect_corners2(img)
+  img = detect_corners3(img)
   img = resize_image(img, 0.2)
   show_image('test', img)
