442391f1cd08b623fc8946e2bd76c4fbfef3b4ee
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-04-05 17:13:18 +0200

    Remove corner detection

--- src/index.py
+++ src/index.py
@@ -3,7 +3,6 @@ import sys
 import glob
 from data.connect import connect_mongodb_database
 from core.visualize import show_image, resize_image
-from core.detection import detect_corners, detect_corners2
 from core.filter import apply_filters
 
 mydb = connect_mongodb_database('localhost', 27017, 'computervision', 'devuser', 'devpwd')
@@ -14,8 +13,6 @@ if (mydb == None):
 
 for filename in glob.glob('./dataset_pictures_msk/*/*.jpg'):
   img = cv2.imread(filename)
-  img = apply_filters(img)
-  # detect_corners(img)
-  # detect_corners2(img)
   img = resize_image(img, 0.2)
+  img = apply_filters(img)
   show_image('test', img)
