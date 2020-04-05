9d2a34e5fb037d61099d1932eb707324ca5a90cf
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-04-05 17:14:39 +0200

    Finetune painting detection

--- src/core/filter.py
+++ src/core/filter.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import math
+from core.visualize import resize_image
 
 def apply_filters(image):
   """
@@ -10,33 +11,36 @@ def apply_filters(image):
   ----------
   - image -- The image to apply filters to.
   """
+  original = image.copy()
+  kernel = np.ones((3,3), np.uint8)
 
-  original = image
-  grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-  kernel = np.ones((5,5), np.uint8)
-  image = cv2.dilate(grayscale, kernel, iterations=1)
-
-  # image = cv2.GaussianBlur(src=image, ksize=(17, 17), sigmaX=0)
-  image = cv2.medianBlur(image, 27)
-
-  image = cv2.Canny(image, threshold1=25, threshold2=80, apertureSize=3)
   image = cv2.dilate(image, kernel, iterations=1)
+  image = cv2.medianBlur(image, 11)
+  image = cv2.dilate(image, kernel, iterations=3)
+  image = resize_image(image, 0.04)
+  image = resize_image(image, 1/0.04)
 
-  # image = cv2.bitwise_not(image)
-
-  contours, hierarchy = cv2.findContours(image, mode = cv2.RETR_EXTERNAL, method = cv2.CHAIN_APPROX_SIMPLE)
+  image = cv2.Canny(image, threshold1=60, threshold2=80, apertureSize=3)
+  image = cv2.dilate(image, kernel, iterations=1)
+  
+  contours, hierarchy = cv2.findContours(image, mode = cv2.RETR_EXTERNAL, method = cv2.CHAIN_APPROX_NONE)
   print("Number of Contours found = " + str(len(contours)))
   
-  ## original = cv2.drawContours(original, contours, -1, (255, 255, 0), 3)
+  # original = cv2.drawContours(original, contours, -1, (255, 255, 0), 3, hierarchy=hierarchy, maxLevel=2)
 
+  area = image.shape[0]*image.shape[1]
   i=0
-  ## blank_image = np.zeros((image.shape[0],image.shape[1],3), np.uint8)
+  count=0
   for c in contours:
-    if(hierarchy[0,i,3] == -1):
-      x, y, w, h = cv2.boundingRect(c)
-      original = cv2.rectangle(original, (x, y), (x+w, y+h), (0, 255, 0), 14)
+    x, y, w, h = cv2.boundingRect(c)
+    aspect_ratio = w/h
+    allowed_aspect_ratio = 8
+    if(hierarchy[0,i,3] == -1 and 1/allowed_aspect_ratio <= aspect_ratio and aspect_ratio <= allowed_aspect_ratio):
+      count += 1
+      original = cv2.rectangle(original, (x, y), (x+w, y+h), (0, 255, 0), 4)
     i += 1
+  print(count)
 
-  
   return original
