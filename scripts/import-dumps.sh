c3134d2150805267f0b19e567b052d401b416e4d
Thomas Aelbrecht <thomasaelbrecht@live.com>
2020-04-17 11:15:47 +0200

    Make MongoDB fill database when empty.

--- scripts/import-dumps.sh
+++ scripts/import-dumps.sh
@@ -2,6 +2,8 @@ DATABASE='cv-temp'
 COLLECTION='images' # necessary for JSON files
 OUTPUT_DIR='out'
 DUMP_FOLDER='mongo-dump'
+USERNAME='devuser'
+PASSWORD='devpwd'
 
 ##############
 # BSON FILES #
@@ -12,7 +14,7 @@ for FILE in dump_*.zip; do
     unzip $FILE -d $OUTPUT_DIR
 
     # restore the BSON data in the specified database
-    mongorestore -d $DATABASE $OUTPUT_DIR/$DUMP_FOLDER
+    mongorestore -d $DATABASE -u $USERNAME -p $PASSWORD --authenticationDatabase=admin $OUTPUT_DIR/$DUMP_FOLDER
 
     # remove all created data
     rm -rf $OUTPUT_DIR
@@ -23,5 +25,7 @@ done
 ##############
 for FILE in dump_*.json; do
     echo "==> Processing $FILE"
-    mongoimport -d $DATABASE -c $COLLECTION --file $FILE
+    mongoimport -d $DATABASE -u $USERNAME -p $PASSWORD --authenticationDatabase=admin -c $COLLECTION --file $FILE
 done
+
+mongodump --db $DATABASE -u $USERNAME -p $PASSWORD --authenticationDatabase=admin
