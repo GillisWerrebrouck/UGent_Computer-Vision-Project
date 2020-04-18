d208f83e89a15bffb8ccb7c557033f1220c44ba5
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-04-18 17:17:13 +0200

    Add initial line detection

--- src/core/detection.py
+++ src/core/detection.py
@@ -95,3 +95,29 @@ def pop_contour_with_id(point, contours):
       contours.remove(c)
       return copy
   return None
+
+def detect_straight_lines(image):
+  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+  
+  edges = cv2.Canny(gray, 50, 150, apertureSize=3) 
+  
+  lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
+
+  if lines is None:
+    return image
+
+  for line in lines:
+    r,theta = line[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*r
+    y0 = b*r
+    
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+
+    cv2.line(image,(x1,y1), (x2,y2), (0,0,255),2) 
+
+  return image
---
+++ src/tasks/task02.py
@@ -0,0 +1,15 @@
+import cv2
+from glob import glob
+
+from core.detection import detect_straight_lines
+from core.visualize import resize_image, show_image
+
+
+def run_task_02():
+  filenames = glob('./datasets/images/dataset_pictures_msk/zaal_*/*.jpg')
+
+  for f in filenames:
+    image = cv2.imread(f, 1)
+    image = resize_image(image, 0.2)
+    image = detect_straight_lines(image)
+    show_image("TEST", image)
