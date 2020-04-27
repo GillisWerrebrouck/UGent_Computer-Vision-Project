c799088b49f552b7d361b6f4aa29381436509c9b
Thomas Aelbrecht <thomasaelbrecht@live.com>
2020-04-27 12:09:37 +0200

    Removed useless comments in accuracy helpers.

--- src/core/accuracyHelperFunctions.py
+++ src/core/accuracyHelperFunctions.py
@@ -1,11 +1,5 @@
 from shapely.geometry import Polygon
 
-# polygon = Polygon([[0, 0], [4, 0], [4, 4], [0, 4], [0, 0]])
-# polygon2 = Polygon([(2, 2), (6, 2), (6, 6), (2, 6), (2, 2)])
-
-# x = polygon.intersection(polygon2)
-# print(x)
-
 def calculate_bounding_box_accuracy(pointList1, pointList2):
     polygon = Polygon(pointList1)
     polygon2 = Polygon(pointList2)
