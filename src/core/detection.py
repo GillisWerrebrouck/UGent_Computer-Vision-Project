b384263b3cb0852a6d8f2d927ace7dc33789c690
Gillis Werrebrouck <gillis-wer@hotmail.be>
2020-05-02 18:46:58 +0200

    Improve painting detection algorithm

--- src/core/detection.py
+++ src/core/detection.py
@@ -97,21 +97,47 @@ def pop_contour_with_id(point, contours):
       return copy
   return None
 
-def detect_quadrilaters(image):
-  grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-  grayscale = cv2.bilateralFilter(grayscale, 1, 10, 120)
-  edges = cv2.Canny(grayscale, 10, 250, apertureSize=3)
+
+def detect_quadrilaters(original_image):
+  image = cv2.pyrMeanShiftFiltering(original_image, 12, 18, maxLevel=4)
+  (h, w, channels) = image.shape
+  mask = np.zeros((h+2, w+2), np.uint8)
+
+  floodFlags = 4
+  floodFlags |= cv2.FLOODFILL_MASK_ONLY
+  floodFlags |= (255 << 8)
+
+  # use flooding to find mask
+  largest_segment_size = 0
+  largest_mask = mask
+  step = 25
+  for y in range(0, image.shape[0], step):
+    for x in range(0, image.shape[1], step):
+      num, im, mask, rect = cv2.floodFill(image, mask, (x, y), (255,0,0), (8,)*3, (8,)*3, floodFlags)
+      x, y, w, h = rect
+      current_size = w*h
+      if largest_segment_size < current_size:
+        largest_segment_size = current_size
+        largest_mask = mask
 
   kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
-  closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+  mask = cv2.bitwise_not(mask)
+  mask = cv2.erode(mask, kernel, 1)
+  mask = cv2.medianBlur(mask, 9)
+  # cv2.imshow('mask', mask)
+
+  # Calculate OTSU threshold to use as threshold for Canny detection
+  grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  ret, threshold = cv2.threshold(grayscale, 0, 255, cv2.THRESH_OTSU)
+  edges = cv2.Canny(mask, ret/2, ret, apertureSize=3)
+  # cv2.imshow('canny', edges)
 
   kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-  closed = cv2.erode(closed, kernel)
+  closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+  # cv2.imshow('morphologyEx', closed)
 
-  kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (11, 11))
-  closed = cv2.dilate(closed, kernel)
-
-  contours, hierarchy = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+  # find contours in mask
+  contours, hierarchy = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
   quadrilaterals = []
 
   (height, width) = image.shape[:2]
@@ -123,7 +149,39 @@ def detect_quadrilaters(image):
 
     if (len(approx) == 4):
       polygon = Polygon(np.reshape(approx, (4, 2)))
-      if(polygon.is_valid and polygon.area/polygonImage.area > 0.1):
+      if(polygon.is_valid and polygon.area/polygonImage.area > 0.005):
         quadrilaterals.append(approx)
 
   return quadrilaterals
+
+
+# def detect_quadrilaters(image):
+#   grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#   grayscale = cv2.bilateralFilter(grayscale, 1, 10, 120)
+#   edges = cv2.Canny(grayscale, 10, 250, apertureSize=3)
+
+#   kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
+#   closed = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel)
+
+#   # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+#   # closed = cv2.erode(closed, kernel)
+
+#   # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (11, 11))
+#   # closed = cv2.dilate(closed, kernel)
+
+#   contours, hierarchy = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#   quadrilaterals = []
+
+#   (height, width) = image.shape[:2]
+#   polygonImage = Polygon([(0, 0), (width, 0), (width, height), (0, height), (0, 0)])
+
+#   for contour in contours:
+#     arc_len = cv2.arcLength(contour, True)
+#     approx = cv2.approxPolyDP(contour, 0.1 * arc_len, True)
+
+#     if (len(approx) == 4):
+#       polygon = Polygon(np.reshape(approx, (4, 2)))
+#       if(polygon.is_valid and polygon.area/polygonImage.area > 0.1):
+#         quadrilaterals.append(approx)
+
+#   return quadrilaterals
--- src/tasks/task02.py
+++ src/tasks/task02.py
@@ -8,9 +8,13 @@ from core.visualize import resize_image, show_image, draw_quadrilaterals_opencv
 from data.imageRepo import get_paintings_for_image
 from core.accuracyHelperFunctions import calculate_bounding_box_accuracy
 
-
 def run_task_02():
   filenames = glob('./datasets/images/dataset_pictures_msk/zaal_*/*.jpg')
+
+  false_negatives_sum = 0
+  false_positives_sum = 0
+  average_accuracy_sum = 0
+  count = 0
 
   for f in filenames:
     image = cv2.imread(f, 1)
@@ -18,7 +22,7 @@ def run_task_02():
     quadrilaterals = detect_quadrilaters(image)
     image = draw_quadrilaterals_opencv(image, quadrilaterals)
     result = get_paintings_for_image(basename(f)) 
-    # todo: misschien enkel de corners ophalen
+    # todo: maybe just fetch the corners
     (height, width) = image.shape[:2]
     false_positives = 0
     false_negatives = 0
@@ -40,16 +44,29 @@ def run_task_02():
         # TODO: check on duplicates, remove from result?
         paintings_found += 1
         average_accuracy += area
+        if area == 0:
+          false_positives += 1
 
     # average accuracy dividing by amount found + calculating the amount of false nefatives 
     # division by 1 is not needed
-    if(paintings_found > 1):
+    if paintings_found > 1:
       average_accuracy /= paintings_found
-    false_positives = len(quadrilaterals) - paintings_found
+    if len(quadrilaterals) >= paintings_found:
+      false_positives = len(quadrilaterals) - paintings_found
 
     # might give non correct result if a quadrilateral contains more than 1 painting!
-    print("false negatives: ", false_negatives)
-    print("false positives: ", false_positives)
+    print("# of false negatives: ", false_negatives)
+    print("# of false positives: ", false_positives)
     print("average bounding box accuracy: ", average_accuracy)
 
-    show_image("DOBRA DOBRA", image)
+    false_negatives_sum += false_negatives
+    false_positives_sum += false_positives
+    average_accuracy_sum += average_accuracy
+    count += 1
+
+    show_image(f, image)
+
+  print("SUMMARY:")
+  print("total # of false negatives: ", false_negatives_sum)
+  print("total # of false positives: ", false_positives_sum)
+  print("average bounding box accuracy: ", average_accuracy_sum/count)
